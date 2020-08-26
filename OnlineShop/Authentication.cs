4ed9e6d3fd6a3a9c82da63c5b5bd30789c216f85
Filipe Cancelinha <48698009+fcancelinha@users.noreply.github.com>
2020-08-26 21:12:49 +0100

    Removed private keys

--- OnlineShop/Authentication.cs
+++ OnlineShop/Authentication.cs
@@ -17,23 +17,17 @@ namespace OnlineShop
              OAuthManager.RegisterClient(
 
                         "google",
-                        "471126422232-va6ngpf754qqf1m3qi48tb0ogqi1gadn.apps.googleusercontent.com",
-                        "2nY8CWl2E10QTrDcaITg8x10"
+                        "***********",
+                        "***********"
                 );
 
             OAuthManager.RegisterClient(
 
                         "facebook",
-                        "303591377373779",
-                        "a1c351552ae7b285bd410c72b5db0524"
+                        "***********",
+                        "***********"
                 );
-
-            OAuthManager.RegisterClient(
-
-                        "github",
-                        "10d3bb7da759c244e45c",
-                        "e59d4c4df61e645f3123d3ab67e6ebc45e386454"
-               );
+            
         }
 
         public static bool registry { get; set; } = false;
